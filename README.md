83b8020cfc48614a5cc0a1d41c6d626a807970f8
Guillermo Lopez <guillermolopez2529@gmail.com>
2018-05-24 11:15:22 -0400

    Basic implementation

---
+++ README.md
@@ -0,0 +1,29 @@
+# opery-sequelize-user-auth-service
+
+> Sequelize user authentication service for opery
+
+
+## Installation
+
+```bash
+npm install --save opery-sequelize-user-auth-service
+```
+
+## Requirements
+
+- You need a Sequelize User Model to use this service.
+- Sequelize User Model should have an username field.
+
+## API
+
+### User
+
+#### `db.models.User.authenticate(username: String, password: String) -> Boolean`
+Try to find an user to authenticate
+
+#### `db.models.User.gerUserByUsername(username: String) -> Boolean`
+Try to find an user by his username
+
+## License
+
+MIT © [Guillermo Lopez](http://www.guillermolopez.net)
---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = function user (Model) {
+  return {
+    async getUserByUsername (username, callback) {
+      try {
+        if (!username) {
+          throw new Error('username is empty')
+        }
+
+        const user = await Model.findOne({ where: { username } })
+
+        if (!user) {
+          throw new Error('user not found')
+        }
+
+      } catch (e) {
+        return Promise.reject(e)
+      }
+    },
+
+    async authenticate (username, password, callback) {
+      try {
+        if (!username || !password) {
+          return Promise.reject(new Error('username or password is empty'))
+        }
+
+        const user = await this.getUserByUsername(username)
+
+        if (user.get('password') !== utils.encrypt(password)) {
+          throw new Error('username or password incorrect')
+        }
+
+        return Promise.resolve(true)
+      } catch (e) {
+        return Promise.resolve(false)
+      }
+    }
+  }
+}
+
+
